77206c66a56b24933868ad3d3fadec0304b3f9d4
HANhuiyu <36294954+HANhuiyu@users.noreply.github.com>
2021-07-01 10:34:52 +0200

    Update two_fans.py

--- examples/two_fans.py
+++ examples/two_fans.py
@@ -22,11 +22,11 @@ def run_experiment_fans(with_visualization=True):
     """
     experiment_name = "Back_Front"
     experiment_duration = 10000.0  # in ms
-    dx = 20  # in pixels
-    dy = 20  # in pixels
-    max_d = 15  # in pixels
-    crop_xmin = 89  # in pixels
-    crop_ymin = 48  # in pixels
+    dx = 92  # in pixels
+    dy = 92  # in pixels
+    max_d = 24  # in pixels
+    crop_xmin = 25  # in pixels
+    crop_ymin = 10  # in pixels
 
     # Setup the simulation
     Simulation = SNNSimulation(simulation_time=experiment_duration)
